46b9127b73f175091d67b96d82506e29befe6607
maanethdesilva <maaneth.2002@gmail.com>
2022-12-01 22:52:25 -0500

    Fixed landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,25 +23,42 @@ export default function Home() {
     <>
       <Navbar />
       <div className="flex justify-between md:justify-right md:space-x-20 bg-navBarBg">
-      <div className="flex-col justify-between">
-        <div className="p-10 pb-10 w-3/4">
-          <a
-            href="#"
-            className="text-base font-extrabold text-white p-0 text-6xl"
-          >
-            Some sort of Slogan to represent us
-          </a>
+        <div className="flex-col justify-between pl-20">
+          <div className="pt-20 pb-10 w-full">
+            <p className="text-base font-extrabold text-white p-0 text-6xl">
+              Some sort of Slogan to represent us
+            </p>
           </div>
+
+          <p className="text-base font-medium text-white pb-10 text-sm">
+            Smaller thing over here
+          </p>
+          <div className="flex justify-between md:justify-right md:space-x-5 bg-navBarBg">
+          <input className="h-11 shadow appearance-none border rounded w-full text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="text" type="text" 
+          placeholder="Enter your email address" />
+
             <a
               href="#"
-              className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-navBarBtnBg px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-navBarBtnBgHover"
+              className="inline-flex 
+            items-center 
+            justify-center 
+            whitespace-nowrap 
+            rounded-md border 
+            border-transparent 
+            bg-navBarBtnBg 
+            mb-10
+            px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-navBarBtnBgHover"
             >
-              Register
+              Subscribe
             </a>
+          </div>
+          <p className="text-base font-medium text-white pb-20 text-xs">
+            Smaller thing over here
+          </p>
         </div>
         <img
-          className="h-60 py-0 sm:h-64 xl:h-96 2xl:h-96"
-          src="https://images.unsplash.com/photo-1454496522488-7a8e488e8606?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2076&q=80"
+          className="w-6/12"
+          src="https://images.pexels.com/photos/417173/pexels-photo-417173.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
           alt="..."
         />
       </div>
