0a45675d24eabf139b59d634006c5758aeb8f506
maanethdesilva <maaneth.2002@gmail.com>
2022-12-01 22:22:42 -0500

    Added to landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,19 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {
       colors: {
-        "dark": "#003381"
+        "navBarBg": "#003381",
+        "navBarBtnBg": "#00BFBF",
+        "navBarBtnBgHover": "#33CDCD"
       }
 
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
