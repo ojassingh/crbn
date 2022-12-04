509f121ed5ce4e022a654a4c39f34c2fd9aa4030
maanethdesilva <maaneth.2002@gmail.com>
2022-12-03 22:49:51 -0500

    changed photo

--- pages/StoreFront.tsx
+++ pages/StoreFront.tsx
@@ -7,8 +7,8 @@ export default function StoreFront() {
     <>
       <Navbar />
       <img 
-        className="filter brightness-50 h-storeImg w-full"
-        src="https://assets.theoceancleanup.com/app/uploads/2021/08/Large-210823_Oceans_Systems_02_Drone_DL8-scaled.jpg"
+        className="filter brightness-50 h-storeImg w-full object-cover"
+        src="https://images.pexels.com/photos/847393/pexels-photo-847393.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
       />
       <ProductList />
     </>
