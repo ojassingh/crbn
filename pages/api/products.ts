023bb3f096137c8699f1eeb9513bda45f3f25482
Muntaqa Mahmood <69706702+muntaqamahmood@users.noreply.github.com>
2022-12-06 18:27:47 -0500

    Update products

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -8,6 +8,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const allPosts = await prisma.product.findMany()
-  res.status(200).json(allPosts)
+  const allProducts = await prisma.product.findMany()
+  res.status(200).json(allProducts)
 }
