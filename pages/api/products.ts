e173d004754a6e8bda1e602c7e2075f05c37909a
Muntaqa Mahmood <69706702+muntaqamahmood@users.noreply.github.com>
2022-12-06 18:27:11 -0500

    Added api files for Products, Stores & Users.

---
+++ pages/api/products.ts
@@ -0,0 +1,13 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const allPosts = await prisma.product.findMany()
+  res.status(200).json(allPosts)
+}
