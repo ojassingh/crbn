45f358c2c385fc27a8ac0498459a3ecc84268abc
maanethdesilva <maaneth.2002@gmail.com>
2022-12-07 14:14:31 -0500

    Removed latency in landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,8 @@ import LandingPage from '../components/LandingPage'
 import Navbar from '../components/Navbar'
 import ProductList from '../components/ProductList'
 import ProductPreview from '../components/ProductPreview'
-import useSWR from 'swr'
-import axios from 'axios'
-import StoreFront from './StoreFront'
-
-const fetcher = (url: string) => axios.get(url).then(res => res.data)
 
 export default function Home() {
-
-  const { data, error } = useSWR('/api/posts', fetcher)
-  if (error) return <div>An error occured.</div>
-  if (!data) return <div>Loading ...</div>
-
-  console.log(data)
-
-  
   return (
     <>
       <Navbar />
