751609736efe18fe462def9cef683aea3deebabc
maanethdesilva <maaneth.2002@gmail.com>
2022-12-09 13:02:10 -0500

    Added styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -27,15 +27,31 @@ export default function Navbar() {
           >
             CRBN
           </a>
-          <div className="flex justify-end space-x-10 lg:w-0 lg:flex-1">
+          <div className="flex justify-end space-x-4 lg:w-0 lg:flex-1">
+            <a className="text-center justify-center space-y-1" href="../">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1}
+                stroke="currentColor"
+                className="w-8 h-8"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+                />
+              </svg>
+            </a>
             <a className="text-center space-y-1" href="../">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                strokeWidth={2}
+                strokeWidth={1}
                 stroke="currentColor"
-                className="w-6 h-6 mx-4"
+                className="w-8 h-8"
               >
                 <path
                   strokeLinecap="round"
@@ -43,31 +59,26 @@ export default function Navbar() {
                   d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
                 />
               </svg>
-              <p className="text-xs">
-                WISHLIST
-              </p>
             </a>
-            <a className="text-center space-y-1" href="../">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={2}
-              stroke="currentColor"
-              className="w-6 h-6 mx-4"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-              />
-            </svg>
-            <p className="text-xs">
-                PROFILE
-              </p>
+            <a className="text-center space-y-1" href="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1}
+                stroke="currentColor"
+                className="w-8 h-8"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                />
+              </svg>
             </a>
           </div>
         </div>
+        <hr />
       </div>
     </Popover>
   )
